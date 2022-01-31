149fd6ed9af2d68b23e47980072dc4a126f5113e
Jason Botesazan <jbotesazan@gmail.com>
2022-01-30 21:15:30 -0500

    commit tree 421f379045be1efe9f1f5400f83c70032fd48d9a

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,16 @@
+class StudentsController < ApplicationController
+    def index
+        students = Student.all
+        render json: students
+    end
+
+    def grades
+        grades = Student.all.order(grade: :desc)
+        render json: grades
+    end
+
+    def highest_grade
+        highest_grade = Student.all.max_by { |x| x.grade }
+        render json: highest_grade
+    end
+end
